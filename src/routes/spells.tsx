397751a9fcc3662d9c52d560b5a3fa6b76398577
Ilya Babanov <ilbabanov@gmail.com>
2025-04-20 11:42:13 +0400

    add router

---
+++ src/routes/spells.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/spells")({
+  component: About,
+});
+
+function About() {
+  return <div>Hello from About!</div>;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import biomePlugin from "vite-plugin-biome";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-    plugins: [
-        react({ tsDecorators: true, devTarget: "esnext" }),
-        biomePlugin(),
-        svgr(),
-    ],
-    css: {
-        devSourcemap: true,
-        transformer: "lightningcss",
-    },
-    server: {
-        open: true,
-    },
-    build: {
-        cssMinify: "lightningcss",
-    },
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react({ tsDecorators: true, devTarget: "esnext" }),
+    biomePlugin(),
+    svgr(),
+  ],
+  css: {
+    devSourcemap: true,
+    transformer: "lightningcss",
+  },
+  server: {
+    open: true,
+  },
+  build: {
+    cssMinify: "lightningcss",
+  },
 });
